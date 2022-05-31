a3306532ece7155d40bd6250dbf09e718a44275d
maicol torres <maicolt401@gmail.com>
2022-05-31 22:19:56 +0200

    create thunks whit connection to data base and redux

---
+++ src/redux/types/userTypes.ts
@@ -0,0 +1,13 @@
+export interface UserData {
+  restaurantName: string;
+  username: string;
+  password: string;
+  CIF: String;
+}
+
+export interface ResponseData {
+  restaurantName: string;
+  username: string;
+  password: string;
+  CIF: String;
+}
