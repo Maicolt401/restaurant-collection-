25ef24485bff380a9c77e40585b3f5775744cff9
maicol torres <maicolt401@gmail.com>
2022-05-31 22:35:20 +0200

    elaborate actionRegister on slice

--- src/redux/feature/userSlice.ts
+++ src/redux/feature/userSlice.ts
@@ -17,7 +17,11 @@ const initialState: userState = {
 const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {},
+  reducers: {
+    register: (user, action) => ({ ...action.payload }),
+  },
 });
+
+export const { register: registerActionCreator } = userSlice.actions;
 
 export default userSlice.reducer;
