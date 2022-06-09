ffc946ff09fa2e35186e2120ba9f177d5390b883
maicol torres <maicolt401@gmail.com>
2022-06-09 21:12:29 +0200

    elaborate testing for CreateaController

---
+++ src/components/CreateController/CreateController.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "../../redux/store/store";
+import CreateController from "./CreateController";
+
+describe("Given a CreateController component", () => {
+  describe("When ti should in the document the text `name`", () => {
+    test("Then it show a text `name` its the same to 1", () => {
+      const expectResult = "Name";
+
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <CreateController />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const expectedText = screen.getByText(expectResult);
+
+      expect(expectedText).toBeInTheDocument();
+    });
+  });
+});
